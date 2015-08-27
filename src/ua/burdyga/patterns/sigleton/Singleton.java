4e2fe5f9d34618599be24509c8ad75f377ac9d46
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2015-08-27 12:41:58 +0300

    Patterns - LazyHolder Singleton

---
+++ src/ua/burdyga/patterns/sigleton/Singleton.java
@@ -0,0 +1,20 @@
+package ua.burdyga.patterns.sigleton;
+
+public class Singleton {
+
+    private Singleton() {
+        System.out.println("Creating new instance");
+    }
+
+    public void printSingleton() {
+        System.out.println("Inside print Singleton");
+    }
+
+    public static Singleton getInstance() {
+        return LazyHolder.INSTANCE;
+    }
+
+    private static class LazyHolder {
+        private static final Singleton INSTANCE = new Singleton();
+    }
+}
