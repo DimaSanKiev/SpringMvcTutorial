c9c0eeb521e666b3926531b0961387c4c134ed43
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2015-08-27 12:37:23 +0300

    Patterns - LazySingleton

---
+++ src/ua/burdyga/patterns/sigleton/LazySingleton.java
@@ -0,0 +1,20 @@
+package ua.burdyga.patterns.sigleton;
+
+public class LazySingleton {
+
+    private static volatile LazySingleton instance = null;
+
+    // private constructor
+    private LazySingleton() {
+    }
+
+    public static LazySingleton getInstance() {
+
+        if (instance == null) {
+            synchronized (LazySingleton.class) {
+                instance = new LazySingleton();
+            }
+        }
+        return instance;
+    }
+}
