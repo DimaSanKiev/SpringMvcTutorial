6824cc74bc54ab974b2771817b1dd0d32b335696
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2015-08-27 12:33:45 +0300

    Patterns - EagerSingleton

---
+++ src/ua/burdyga/patterns/sigleton/EagerSingleton.java
@@ -0,0 +1,22 @@
+package ua.burdyga.patterns.sigleton;
+
+public class EagerSingleton {
+
+    private static volatile EagerSingleton instance = null;
+
+    // private constructor
+    private EagerSingleton() {
+    }
+
+    public static EagerSingleton getInstance() {
+
+        if (instance == null) {
+            synchronized (EagerSingleton.class) {
+                if (instance == null) {
+                    instance = new EagerSingleton();
+                }
+            }
+        }
+        return instance;
+    }
+}
