e66a429b5c9bc6f75880daff207318c3bd5f843e
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2015-08-27 16:17:25 +0300

    Spring Core and MVC - Example

--- src/ua/burdyga/_2_spring/beans/HelloWorldBeanDriver.java
+++ src/ua/burdyga/_2_spring/beans/HelloWorldBeanDriver.java
@@ -6,7 +6,7 @@ import org.springframework.context.support.FileSystemXmlApplicationContext;
 public class HelloWorldBeanDriver {
 
     public static void main(String[] args) {
-        ApplicationContext context = new FileSystemXmlApplicationContext("");
+        ApplicationContext context = new FileSystemXmlApplicationContext("resources/spring-example.xml");
         HelloWorld obj = (HelloWorld) context.getBean("helloWorldBean");
         obj.sayHelloWorld();
     }
