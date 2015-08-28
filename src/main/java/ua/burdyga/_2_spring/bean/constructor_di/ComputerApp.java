e21ac181f386a6b9377859f9cc23f4e98c02d475
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2015-08-29 02:24:06 +0300

    Spring Dependency Injection - Spring Constructor-based DI

---
+++ src/main/java/ua/burdyga/_2_spring/bean/constructor_di/ComputerApp.java
@@ -0,0 +1,17 @@
+package ua.burdyga._2_spring.bean.constructor_di;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+public class ComputerApp {
+
+    public static void main(String[] args) {
+        ApplicationContext context = new FileSystemXmlApplicationContext("src/main/resources/spring-example.xml");
+        ComputerBean computer1 = (ComputerBean) context.getBean("computerBean");
+        computer1.specs();
+        System.out.println("\n[Calling 2nd Computer instance] \n");
+
+        ComputerBean computer2 = (ComputerBean) context.getBean("computerBean2");
+        computer2.specs();
+    }
+}
---
+++ src/main/java/ua/burdyga/_2_spring/bean/constructor_di/HardDriveBean.java
@@ -0,0 +1,62 @@
+package ua.burdyga._2_spring.bean.constructor_di;
+
+public class HardDriveBean {
+    private String manufacturer;
+    private String name;
+    private String capacity;
+    private String rotationalSpeed;
+    private String seekSpeed;
+    private String interfaceType;
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(String capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getRotationalSpeed() {
+        return rotationalSpeed;
+    }
+
+    public void setRotationalSpeed(String rotationalSpeed) {
+        this.rotationalSpeed = rotationalSpeed;
+    }
+
+    public String getSeekSpeed() {
+        return seekSpeed;
+    }
+
+    public void setSeekSpeed(String seekSpeed) {
+        this.seekSpeed = seekSpeed;
+    }
+
+    public String getInterfaceType() {
+        return interfaceType;
+    }
+
+    public void setInterfaceType(String interfaceType) {
+        this.interfaceType = interfaceType;
+    }
+
+    public void specs() {
+        System.out.println("Hard Drive specs.");
+    }
+}
