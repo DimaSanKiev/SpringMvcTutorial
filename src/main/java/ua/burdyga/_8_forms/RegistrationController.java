dc0d40ce0f02a8e33834de1ed5ce96c82381d9d8
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2015-09-02 02:08:36 +0300

    Forms - Form Handling

---
+++ src/main/java/ua/burdyga/_8_forms/RegistrationController.java
@@ -0,0 +1,49 @@
+package ua.burdyga._8_forms;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value = "/registration")
+public class RegistrationController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String showForm(ModelMap model) {
+        User user = new User();
+        model.addAttribute("USER", user);
+        return "registration";
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public String processForm(@ModelAttribute(value = "USER") User user, BindingResult result) {
+        if (result.hasErrors()) {
+            return "registration";
+        } else {
+            System.out.println("User value is: " + user);
+            return "success";
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
